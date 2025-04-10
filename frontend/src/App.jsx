ff99f1b35ee474db052c2d0b74270307c00cf5e9
Sumit Jadhav <sumitjadav2525@gmail.com>
2025-04-10 12:11:34 +0530

    frontend with three_js

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { App as Canvas } from './Canvas'
+import { Overlay } from './Overlay'
+
+function App() {
+  return (
+    <>
+      <Canvas />
+      <Overlay />
+    </>
+  )
+}
+
+export default App
